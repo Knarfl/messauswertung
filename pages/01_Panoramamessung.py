218be2fa4f81dacdf05ae03be94ff6d9a4c93488
Knarfl <meinelt-franz@web.de>
2023-01-10 18:58:01 +0100

    'update'

--- pages/01_Panoramamessung.py
+++ pages/01_Panoramamessung.py
@@ -41,8 +41,11 @@ if authentication_status is None:
 
 if authentication_status:
     st.sidebar.write(f"Herzlich Willkommen {name}")     # shows logged user
-    pwd = st.sidebar.text_input()
-    db.update_pwd(username, pwd)
+    pwd = st.sidebar.text_input("Passwort ändern", "neues Password")
+
+    if st.button('Bestätigen'):
+        db.update_pwd(username, pwd)
+
     authenticator.logout("Logout", "sidebar")           # Logout Button
     # file uploader field
     uploaded_files = st.sidebar.file_uploader("Wähle Excel-Dateien aus", accept_multiple_files=True, type='xls')
