99cf3dcae59e0a8cb2b845764870bb96c5390fe0
Knarfl <meinelt-franz@web.de>
2023-01-11 14:53:47 +0100

    'update'

--- pages/01_Panoramamessung.py
+++ pages/01_Panoramamessung.py
@@ -190,7 +190,7 @@ if authentication_status:
 
                 template_img = drive.get_file('Exportvorlage.png')
                 template_img = Image.open(template_img)
-                st.image(template_img, width=250)
+                st.image(template_img, width=750)
 
             else:
                 st.info("Es wurden keine Daten eingelesen")
